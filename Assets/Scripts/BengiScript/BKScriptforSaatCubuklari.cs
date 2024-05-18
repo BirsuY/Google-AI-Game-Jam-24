1b16e4e77100ce17af4deaa53c1fe02d6ec7c776
Bengilata <130096478+Bengilata@users.noreply.github.com>
2024-05-18 23:05:15 +0300

    sahne3Tamamlandı

--- Assets/Scripts/BengiScript/BKScriptforSaatCubuklari.cs
+++ Assets/Scripts/BengiScript/BKScriptforSaatCubuklari.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BKScriptforSaatCubuklari : MonoBehaviour
 {
-    [SerializeField] float cubukDonmesi = 0.1f;
+    [SerializeField] float cubukDonmesi;
+    [SerializeField] float tersCubukDonmesi;
 
     void Update()
     {
         transform.Rotate(Vector3.forward, -cubukDonmesi * Time.deltaTime * 3600);
+        transform.Rotate(Vector3.forward, tersCubukDonmesi * Time.deltaTime * 3600);
     }
 
 
