312d24caaf68be95947e27d601c16a47a2b4b60f
Bengilata <130096478+Bengilata@users.noreply.github.com>
2024-05-19 02:03:03 +0300

    sahne geçişleri tamamlandı, karakter anim vs eklenme aşamasında

--- Assets/Scripts/BengiScript/GameManager.cs
+++ Assets/Scripts/BengiScript/GameManager.cs
@@ -5,9 +5,47 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    [SerializeField] float timeToSurvive = 10f; 
+    private float timer;
+    private bool gameStarted;
+    private int activeSceneIndex = 1;
+
+    void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        Debug.Log("geçti");
+        activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        timer = 0f;
+        gameStarted = true; 
+        Debug.Log("Oyun Baþladý");
     }
 
+    void Update()
+    {
+        if (gameStarted)
+        {
+            timer += Time.deltaTime;
+            Debug.Log("Zamanlayýcý: " + timer);
+
+            if (timer >= timeToSurvive)
+            {
+                
+                int nextSceneIndex = activeSceneIndex + 1;
+
+                if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+                {
+                    SceneManager.LoadScene(nextSceneIndex);
+                }
+                else
+                {
+                    Debug.Log("Sonraki sahne bulunamadý. Mevcut son sahnedesiniz.");
+                }
+            }
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Sahne Yükleniyor: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
 }
